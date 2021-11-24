4ee9bc9de875057d54ae4d5d08b217ef9649768d
Federico Adria <fadriacarrasco@gmail.com>
2021-11-25 00:27:48 +0100

    RecyclerView: Configuración

--- app/src/main/java/com/feadca/userssp/MainActivity.kt
+++ app/src/main/java/com/feadca/userssp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.feadca.userssp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.feadca.userssp.adapters.UserAdapter
+import com.feadca.userssp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var linearLayoutManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userAdapter = UserAdapter(mutableListOf()) // Inicializamos el adapter
+        linearLayoutManager = LinearLayoutManager(this) // Enviamos el contexto actual
+
+        // RecyclerView que tiene el XML del MainActivity
+        binding.recyclerView.apply{
+            layoutManager = linearLayoutManager
+            adapter = userAdapter
+        }
     }
 }
