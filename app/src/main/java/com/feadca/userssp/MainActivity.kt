8c27368f39e02a67c53cde955677a6cea0909803
Federico Adria <fadriacarrasco@gmail.com>
2021-11-25 00:37:07 +0100

    Generar datos básicos para comprobar el funcionamiento del RecyclerView

--- app/src/main/java/com/feadca/userssp/MainActivity.kt
+++ app/src/main/java/com/feadca/userssp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.feadca.userssp.adapters.UserAdapter
 import com.feadca.userssp.databinding.ActivityMainBinding
+import com.feadca.userssp.model.User
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,13 +19,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        userAdapter = UserAdapter(mutableListOf()) // Inicializamos el adapter
+
+        userAdapter = UserAdapter(getUsers()) // Inicializamos el adapter
         linearLayoutManager = LinearLayoutManager(this) // Enviamos el contexto actual
+
 
         // RecyclerView que tiene el XML del MainActivity
         binding.recyclerView.apply{
             layoutManager = linearLayoutManager
             adapter = userAdapter
         }
+    }
+
+    private fun getUsers(): MutableList<User>
+    {
+        val users = mutableListOf<User>()
+
+        val user1 = User(1, "Antonio", "Adria", "")
+        val user2 = User(2, "Federico", "Adria", "")
+
+        users.add(user1)
+        users.add(user2)
+
+        return users
     }
 }
